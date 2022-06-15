8aa8d1e5e2d5952c443971a0ad2d7032da20f410
Jihun Moon <anslgns1@gmail.com>
2022-06-15 14:44:50 +0900

    Swagger 적용

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostgresConfigModule } from './config/database/config.module';
+import { PostgresConfigService } from './config/database/config.service';
+import { UsersModule } from './users/users.module';
+import { ScoresModule } from './scores/scores.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [PostgresConfigModule],
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+    UsersModule,
+    ScoresModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService, PostgresConfigService],
+})
+export class AppModule {}
---
+++ src/utils/swagger.ts
@@ -0,0 +1,26 @@
+import { INestApplication } from '@nestjs/common';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
+const swaggerCustomOptions: SwaggerCustomOptions = {
+  swaggerOptions: {},
+};
+
+/**
+ * @author MOON
+ * @description Swagger 설정
+ */
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('PSM API')
+    .setDescription('PSM API 문서')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document, swaggerCustomOptions);
+}
